2b1b8764356b55fbce9278704ef1bffabe801fc9
Tien Do <tiendq@gmail.com>
2022-09-06 14:06:25 +0700

    fix not turn off issue

--- fish_tank/fish_tank.py
+++ fish_tank/fish_tank.py
@@ -2,10 +2,13 @@ import machine
 import neopixel
 import utime
 
-GPIO_PIN = 5
-LED_COUNT = 20
+DIN_PIN = 5
+# LED_ONBOARD_PIN = 25 # Pico
+LED_ONBOARD_PIN = 2 # ESP8266
+# LED_COUNT = 10 # Pico
+LED_COUNT = 20 # ESP8266
 
-pixels = neopixel.NeoPixel(machine.Pin(GPIO_PIN), LED_COUNT)
+pixels = neopixel.NeoPixel(machine.Pin(DIN_PIN), LED_COUNT)
 
 is_led_on = False
 turn_on_hour = 17
@@ -17,34 +20,36 @@ def handle_led_strip(hour):
 
   global is_led_on
 
-  if is_led_on:
-    if hour >= turn_off_hour:
-      is_led_on = False
-      for i in range(LED_COUNT):
-        pixels[i] = (0, 0, 0)
-      pixels.write()
-      print('LED off')
-  else:
+  if is_led_on and hour >= turn_off_hour:
+    is_led_on = False
+    for i in range(LED_COUNT):
+      pixels[i] = (0, 0, 0)
+    pixels.write()
+    print('LED turned off')
+
+  if not is_led_on and hour < turn_off_hour:
     is_led_on = True
     for i in range(LED_COUNT):
       pixels[i] = (225, 225, 225)
     pixels.write()
-    print('LED on')
+    print('LED turned on')
 
 # Note that on method of a Pin might turn the LED off and off might turn it on (or vice versa),
 # depending on how the LED is wired on your board.
-led_onboard = machine.Pin(2, machine.Pin.OUT)
+led_onboard = machine.Pin(LED_ONBOARD_PIN, machine.Pin.OUT)
 
 def start():
   led_onboard.off()
   utime.sleep_ms(500)
   led_onboard.on()  # it actually turns the LED off!
 
+  # It's worth doing a reset since colors might be not displayed correctly.
   for i in range(LED_COUNT):
     pixels[i] = (0, 0, 0)
   pixels.write()
 
-  utc_offset = 7 * 60 * 60
+  # utc_offset = 0 # emulation mode
+  utc_offset = 7 * 60 * 60 # production mode
   while True:
     now = utime.localtime(utime.time() + utc_offset)
     handle_led_strip(now[3])
